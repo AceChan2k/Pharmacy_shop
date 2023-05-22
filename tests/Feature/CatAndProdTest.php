48248bb360763af1fc6c4a900203cb31aba45e8a
AceChan2k <133310349+AceChan2k@users.noreply.github.com>
2023-05-22 19:55:24 +0500

    Update CatAndProdTest.php

--- tests/Feature/CatAndProdTest.php
+++ tests/Feature/CatAndProdTest.php
@@ -16,7 +16,7 @@ class CatAndProdTest extends TestCase
     {
         // Создаем тестовые данные для категории
         $categoryData = [
-            'id' => '2',
+            'id' => '1',
             'title' => 'Новая категория',
             'description' => 'Описание категории',
             'image' => 'Новая фотография',
